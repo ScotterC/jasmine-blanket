a1bb7e3215edf7b93f7e4e4914be816dc3c2d9eb
Scott Carleton <scott@artsicle.com>
2014-08-12 14:17:34 -0400

    first commit

---
+++ lib/jasmine/blanket.rb
@@ -0,0 +1,7 @@
+require "jasmine/blanket/version"
+
+module Jasmine
+  module Blanket
+    # Your code goes here...
+  end
+end
---
+++ lib/jasmine/blanket/version.rb
@@ -0,0 +1,5 @@
+module Jasmine
+  module Blanket
+    VERSION = "0.0.1"
+  end
+end
